7a634089871d25fb016d9045c4ea3098cb825106
LeninNavarro <czymlpnavarro17@gmail.com>
2022-11-19 21:26:49 +0800

    First commit

---
+++ Portfolio/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default function Hero(){
+    return (
+        <>
+            <div className="mx-auto my-auto text-center">
+            <p>Hello World!</p>
+            </div>    
+        
+        
+        
+        </>
+        
+    )
+
+}
